4a88f45160201efa7938a4f7ca7dbee8760eac9e
Shivam Jain <shivam.jain@synapse.com>
2021-01-10 23:22:46 +0530

    update service methods + refactoring

--- angular-demo/src/app/data.config.ts
+++ angular-demo/src/app/data.config.ts
@@ -1,2 +1,2 @@
 export let getAllLists = 'https://api.spaceXdata.com/v3/launches?limit=100';
-export let getLaunchByYear = 'https://api.spaceXdata.com/v3/launches?limit=100&launch_year=';
+// export let getLaunchByYear = 'https://api.spaceXdata.com/v3/launches?limit=100&launch_year=';
