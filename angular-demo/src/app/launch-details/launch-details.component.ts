8847dd0bdfd21dcba3a83a075837db9f03ae6203
Shivam Jain <shivam.jain@synapse.com>
2021-01-10 18:50:28 +0530

    new component for launch details card

---
+++ angular-demo/src/app/launch-details/launch-details.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-launch-details',
+  templateUrl: './launch-details.component.html',
+  styleUrls: ['./launch-details.component.css']
+})
+export class LaunchDetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
