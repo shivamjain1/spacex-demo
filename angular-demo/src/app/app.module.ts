36bf4f66e0d1101549e495adef7a5065a95faa70
Shivam Jain <shivam.jain@synapse.com>
2021-01-10 18:49:51 +0530

    styling filter section

--- angular-demo/src/app/app.module.ts
+++ angular-demo/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { LaunchProgramsComponent } from './launch-programs/launch-programs.component';
+import { LaunchDetailsComponent } from './launch-details/launch-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LaunchProgramsComponent
+    LaunchProgramsComponent,
+    LaunchDetailsComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'serverApp' })
--- angular-demo/src/app/launch-programs/launch-programs.component.ts
+++ angular-demo/src/app/launch-programs/launch-programs.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./launch-programs.component.css']
 })
 export class LaunchProgramsComponent implements OnInit {
+  launchYears = ['2006', '2007', '2008', '2009', '2010', '2011', '2012', '2013', '2014', '2015', '2016', '2017', '2018', '2019', '2020'];
 
   constructor() { }
 
